b261d4164ee236d3b58252222a27453c403aab64
rangel.uzunov <rangel.uzunov@delasport.com>
2025-09-12 13:15:58 +0300

    Add Nelmio Api Doc

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class            => ['all' => true],
-    RedisService\Symfony\RedisServiceBundle::class                   => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class             => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    RedisService\Symfony\RedisServiceBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
