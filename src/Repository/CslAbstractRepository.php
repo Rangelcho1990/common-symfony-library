2c3b841d869db0c31b6a1363f89f921e7608ee8c
Rangel Uzunov <rangelcho1900@gamil.com>
2025-11-12 21:44:02 +0200

    Fix point 4,

--- src/Repository/CslAbstractRepository.php
+++ src/Repository/CslAbstractRepository.php
@@ -14,15 +14,11 @@ use Doctrine\ORM\EntityRepository;
  */
 abstract class CslAbstractRepository extends EntityRepository
 {
-    protected EntityManagerInterface $entityManager;
-
     /**
      * @param class-string<T> $entityClass
      */
     public function __construct(EntityManagerInterface $entityManager, string $entityClass)
     {
         parent::__construct($entityManager, $entityManager->getClassMetadata($entityClass));
-
-        $this->entityManager = $entityManager;
     }
 }
