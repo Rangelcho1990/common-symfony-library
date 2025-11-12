7bafc2657949f715c93dcf90fe12ca84241a650d
Rangel Uzunov <rangelcho1900@gamil.com>
2025-11-12 21:36:26 +0200

    Fix point 3

--- src/Endpoints/Examples/ExampleList/Controller/ExampleController.php
+++ src/Endpoints/Examples/ExampleList/Controller/ExampleController.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 namespace CSL\Endpoints\Examples\ExampleList\Controller;
 
 use CSL\Controller\CslAbstractController;
+use RedisService\Core\Container\RedisContainer;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExampleController extends CslAbstractController
 {
-    #[Route('/users', name: 'users_list', methods: 'GET')]
-    public function list(): JsonResponse
+    #[Route('/example', name: 'example', methods: 'GET')]
+    public function example(RedisContainer $redisContainer): JsonResponse
     {
         return $this->json([
-            'Redis connection' => $this->redisContainer->isConnected(),
+            'Redis connection' => $redisContainer->isConnected(),
         ]);
     }
 }
